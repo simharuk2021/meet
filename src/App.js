036bfc8c85af9696e02c25a0aea9df031bae822d
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-12-22 15:34:39 +0000

    CODE RE-ADDED
    disappearing code re added to app.js

--- src/App.js
+++ src/App.js
@@ -3,50 +3,81 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import Event from './Event';
+// import Event from './Event';
 import NumberOfEvents from'./NumberOfEvents';
 import { getEvents, extractLocations } from './api';
+import './nprogress.css';
+import { Container } from 'react-bootstrap';
 
 
 class App extends Component {
-  state = {
+  constructor(props) {
+    super(props);
+ 
+  this.state = {
   events: [],
-  locations: []
-  }
-
-  componentDidMount() {
-    this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
-  }
+  locations: [],
+  numberOfEvents: 32,
+  currentLocation: 'all'
+  };
+}
+async componentDidMount() {
+  const { numberOfEvents } = this.state;
+  this.mounted = true;
+  getEvents().then((events) => {
+    if (this.mounted) {
+      this.setState({
+        events: events.slice(0, numberOfEvents),
+        locations: extractLocations(events)
+      });
+    }
+  });
+}
 
   componentWillUnmount(){
     this.mounted = false;
   }
-  
-  updateEvents = (location) => {
+
+  updateEvents = async (location, numberOfEvents) => {
     getEvents().then((events) => {
       const locationEvents = (location === 'all') ?
-      events :
-      events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
-      });
+        events :
+        events.filter((event) => event.location === location);
+      if (this.mounted) {
+        this.setState({
+          events: locationEvents.slice(0, this.state.numberOfEvents),
+          currentLocation: location,
+        });
+      }
     });
   }
+
+  updateNumberOfEvents = async (e) => {
+    const newNumber = e.target.value ? parseInt(e.target.value) : 32;
+    if (newNumber < 1 || newNumber > 32) {
+      return this.setState({
+        errorText: 'Please choose a number between 1 and 32.',
+        numberOfEvents: 0,
+      });
+    } else {
+      this.setState({
+        numberOfEvents: newNumber,
+      });
+      this.updateEvents(this.state.currentLocation, this.state.numberOfEvents);
+    }
+  };
+
   render() {
 
     
   return (
-    <div className="App">
+    <Container className="App">
       <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-      <NumberOfEvents />
+      <NumberOfEvents 
+                numberOfEvents={this.state.numberOfEvents} 
+                updateNumberOfEvents={this.updateNumberOfEvents} />
       <EventList events={this.state.events}/>
-      <Event />
-    </div>
+    </Container>
   );
  }
 }
