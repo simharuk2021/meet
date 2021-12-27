fb72253f309f4634cacf7d3bf2b4633d0820e5a9
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-12-27 16:07:25 +0000

    VERIFICATION 1.2

--- src/App.js
+++ src/App.js
@@ -31,8 +31,7 @@ async componentDidMount() {
   const { numberOfEvents } = this.state;
   this.mounted = true;
   const accessToken = localStorage.getItem('access_token');
-const isTokenValid = (await checkToken(accessToken)).error ? false :
-true;
+  const isTokenValid = (await checkToken(accessToken)).error ? false : true;
   const searchParams = new URLSearchParams(window.location.search);
   const code = searchParams.get("code");
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
