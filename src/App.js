89ec07658ae92ff8f8ec93f3f67a9d6f531fdc31
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-12-28 17:01:27 +0000

    CHART CORRECTION

--- src/App.js
+++ src/App.js
@@ -121,8 +121,8 @@ async componentDidMount() {
     <ResponsiveContainer height={300} width="99%" >
       <ScatterChart margin={{top: 20, right: 20, bottom: 20, left: 20,}}>
       <CartesianGrid />
-      <XAxis type="category" dataKey="city" name="city" />
-      <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false}/>
+      <XAxis dataKey="city" type = "category"  name="city" />
+      <YAxis dataKey="number" type="number"  name="number of events" allowDecimals={false}/>
       <Tooltip cursor={{ strokeDasharray: '3 3' }} />
       <Scatter data={this.getData()} fill="#00008B" />
       </ScatterChart>
